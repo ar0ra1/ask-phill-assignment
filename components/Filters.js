d4c33887f85fa16148f29b1b4051eb058fce829b
Akash Arora <akash.arora777@gmail.com>
2022-06-06 15:18:00 +0200

    Added Price + Category Filter + Fixed Price UI bug

--- components/Filters.js
+++ components/Filters.js
@@ -1,6 +1,68 @@
-import React, { memo } from "react";
+import React, { useState, memo, useEffect } from "react";
 
-const Filters = ({ allColors, allCategories }) => {
+const Filters = ({
+  allColors,
+  allCategories,
+  handleColorFilter,
+  handleCategoryFilter,
+  minPriceRange,
+  maxPriceRange,
+  handlePriceFilter,
+  resetFilterProducts,
+}) => {
+  const [userMin, setUserMin] = useState(minPriceRange);
+  const [userMax, setUserMax] = useState(maxPriceRange);
+  const [userInducedChanged, setUserInducedChange] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(new Set());
+  const [selectedColor, setSelectedColor] = useState(new Set());
+
+  useEffect(() => {
+    // only update when the user changes -> FIXES UI bug
+    if (!userInducedChanged) {
+      setUserMax(maxPriceRange);
+      setUserMin(minPriceRange);
+    }
+  }, [maxPriceRange, minPriceRange, userInducedChanged]);
+
+  const onResetClick = () => {
+    setSelectedCategory(new Set());
+    setSelectedColor(new Set());
+    setUserInducedChange(false);
+    resetFilterProducts();
+  };
+
+  const onCategorySelect = (newCategory) => {
+    setSelectedCategory(
+      selectedCategory.has(newCategory)
+        ? new Set(Array.from(selectedCategory).filter((e) => e !== newCategory))
+        : (prev) => new Set([...prev, newCategory])
+    );
+    handleCategoryFilter(newCategory);
+  };
+
+  const onColorSelect = (newColor) => {
+    setSelectedColor(
+      selectedColor.has(newColor)
+        ? new Set(Array.from(selectedColor).filter((e) => e !== newColor))
+        : (prev) => new Set([...prev, newColor])
+    );
+    handleColorFilter(newColor);
+  };
+
+  const onMinPriceChange = (e) => {
+    const newMin = e.target.value;
+    setUserMin(newMin);
+    setUserInducedChange(true);
+    handlePriceFilter([newMin, userMax]);
+  };
+
+  const onMaxPriceChange = (e) => {
+    const newMax = e.target.value;
+    setUserMax(newMax);
+    setUserInducedChange(true);
+    handlePriceFilter([userMin, newMax]);
+  };
+
   return (
     <div className="flex flex-col max-w-sm gap-4 p-10 pb-20 rounded-sm shadow-md bg-secondaryBg h-fit">
       <h4 className="text-2xl font-bold text-orange-700">Filters</h4>
@@ -10,9 +72,12 @@ const Filters = ({ allColors, allCategories }) => {
           <div className="flex flex-row flex-wrap gap-3 my-4">
             {allColors.map((color) => (
               <div
+                onClick={() => onColorSelect(color)}
                 key={color}
                 style={{ backgroundColor: color }}
-                className={`w-7 h-7 rounded-full cursor-pointer drop-shadow-md hover:drop-shadow-xl transition-all ease-in-out delay-150 `}
+                className={`w-7 h-7 rounded-full cursor-pointer drop-shadow-md hover:drop-shadow-xl transition-all ease-in-out delay-150 ${
+                  selectedColor.has(color) && "border-cyan-300 border-2"
+                } `}
               />
             ))}
           </div>
@@ -23,7 +88,11 @@ const Filters = ({ allColors, allCategories }) => {
             {allCategories.sort() &&
               allCategories.map((category) => (
                 <p
-                  className={`cursor-pointer  transition-all ease-in-out delay-150  `}
+                  onClick={() => onCategorySelect(category)}
+                  className={`cursor-pointer  transition-all ease-in-out delay-150 ${
+                    selectedCategory.has(category) &&
+                    "border-b-4 border-orange-400"
+                  } `}
                   key={category}
                 >
                   {category}
@@ -35,20 +104,33 @@ const Filters = ({ allColors, allCategories }) => {
           <h5 className="font-semibold text-gray-500">Price Range</h5>
           <div className="flex flex-row items-center gap-4">
             <input
+              onChange={(e) => onMinPriceChange(e)}
               className="w-20 px-4 py-2 leading-tight text-gray-700 border-b-2 border-gray-500 rounded appearance-none focus:outline-none bg-secondaryBg focus:border-orange-500"
               id="inline-full-name"
               type="number"
+              value={userMin}
             />
             between
             <input
+              onChange={(e) => onMaxPriceChange(e)}
               className="w-20 px-4 py-2 leading-tight text-gray-700 border-b-2 border-gray-500 rounded appearance-none focus:outline-none bg-secondaryBg focus:border-orange-500"
               id="inline-full-name"
               type="number"
+              value={userMax}
             />
           </div>
         </div>
         <div className="flex items-center mt-10">
-          <button className="p-2 text-white bg-orange-700 border-r-2 w-36 hover:bg-orange-900 disabled:bg-orange-200">
+          <button
+            disabled={
+              userMin === minPriceRange &&
+              userMax === maxPriceRange &&
+              selectedCategory.size === 0 &&
+              selectedColor.size === 0
+            }
+            onClick={() => onResetClick()}
+            className="p-2 text-white bg-orange-700 border-r-2 w-36 hover:bg-orange-900 disabled:bg-orange-200"
+          >
             Reset
           </button>
         </div>
--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useMemo, useCallback } from "react";
-import { isValidColor, formatCategories } from "../lib/helper";
+import {
+  isValidColor,
+  formatCategories,
+  checkForExisitngFilters,
+  deleteKeyFromObject,
+  filterOnColor,
+  filterOnCategory,
+  filterOnPrice,
+} from "../lib/helper";
 import { FiltersComponent } from "./Filters";
 import { Product } from "./Product";
 import { Pagination } from "./Pagination";
@@ -11,6 +19,9 @@ export const PageWrapper = ({ data }) => {
   const [allCategories, setAllCategories] = useState(new Set());
   const [page, setPage] = useState(1);
   const [totalPageCount, setTotalPageCount] = useState(0);
+  const [filterProducts, setFilterProducts] = useState({});
+  const [minPriceRange, setMinPriceRange] = useState(Infinity);
+  const [maxPriceRange, setMaxPriceRange] = useState(0);
 
   const edges = data.data.allContentfulProductPage.edges;
 
@@ -51,23 +62,108 @@ export const PageWrapper = ({ data }) => {
     }
   }, [page]);
 
+  // Filter Handlers
+  const colorFilterCallBack = useCallback(
+    (newColor) => {
+      const existing = checkForExisitngFilters(
+        "Color",
+        newColor,
+        filterProducts
+      );
+      if (existing.size === 0) {
+        setFilterProducts((prev) => deleteKeyFromObject("Color", { ...prev }));
+      } else {
+        setFilterProducts((prev) => ({
+          ...prev,
+          ["Color"]: Array.from(existing),
+        }));
+      }
+      setPage(1);
+    },
+    [filterProducts]
+  );
+
+  const categoryFilterCallBack = useCallback(
+    (newCategory) => {
+      const existing = checkForExisitngFilters(
+        "Category",
+        newCategory,
+        filterProducts
+      );
+      if (existing.size === 0) {
+        setFilterProducts((prev) =>
+          deleteKeyFromObject("Category", { ...prev })
+        );
+      } else {
+        setFilterProducts((prev) => ({
+          ...prev,
+          ["Category"]: Array.from(existing),
+        }));
+      }
+      setPage(1);
+    },
+    [filterProducts]
+  );
+
+  const priceFilterCallBack = useCallback((newPrice) => {
+    setPage(1);
+    setFilterProducts((prev) => ({ ...prev, ["Price"]: [...newPrice] }));
+  }, []);
+
+  const applyFilters = useCallback(() => {
+    return Object.entries(filterProducts).reduce((acc, [key, value]) => {
+      return acc.filter((e) => {
+        switch (key) {
+          case "Color":
+            return filterOnColor(value, e);
+          case "Category":
+            return filterOnCategory(value, e);
+          case "Price":
+            return filterOnPrice(value, e);
+        }
+      });
+    }, edges);
+  }, [filterProducts, edges]);
+
+  // reset filters
+  const resetFilterProducts = useCallback(() => {
+    setFilterProducts({});
+  }, []);
+
+  // update min and max for each filter change
+  const updatePrice = useCallback((data) => {
+    const allPrices = data.map(
+      (o) => o.node.shopifyProductEu.variants.edges[0].node.price
+    );
+    setMinPriceRange(Math.min(...(allPrices || 0)));
+    setMaxPriceRange(Math.max(...allPrices) || 0);
+  }, []);
+
   // products to display handler
   // memo for future extendability
   const allProductsToDisplay = useMemo(() => {
-    const dataToDisplay = edges;
+    const dataToDisplay =
+      Object.keys(filterProducts).length !== 0 ? applyFilters(edges) : edges;
     const startingIndex = (page - 1) * PAGE_SIZE;
     const endingIndex = startingIndex + PAGE_SIZE;
+    updatePrice(dataToDisplay);
     setTotalPageCount(Math.ceil(dataToDisplay.length / PAGE_SIZE));
     // sort -> for future extendability
     dataToDisplay.sort((a, b) => a.node.name.localeCompare(b.node.name));
     return dataToDisplay.slice(startingIndex, endingIndex);
-  }, [page, edges]);
+  }, [page, edges, filterProducts, applyFilters, updatePrice]);
 
   return (
     <div className="flex flex-row gap-7">
       <FiltersComponent
+        handleCategoryFilter={categoryFilterCallBack}
+        handleColorFilter={colorFilterCallBack}
         allColors={memoizedAllColors}
         allCategories={memoizedAllCategories}
+        minPriceRange={minPriceRange}
+        maxPriceRange={maxPriceRange}
+        handlePriceFilter={priceFilterCallBack}
+        resetFilterProducts={resetFilterProducts}
       />
       <div className="flex flex-col grow-1">
         <Product data={allProductsToDisplay} />
